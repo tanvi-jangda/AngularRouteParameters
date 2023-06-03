1254f93741a75655d30d54744c900a62d75295bc
tanvi-jangda <tanvi.jangda@gmail.com>
2023-06-03 22:15:51 +0530

    route parameter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { DepartmentDetailComponent } from './department-detail/department-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentListComponent,
+    DepartmentDetailComponent
   ],
   imports: [
     BrowserModule,
